6ef2f3d25d12ac930814fcb0987770b26ccf1e29
creitzel <creitzel>
2001-08-06 22:50:30 +0000

    Test scripts to run tidy w/ config options for test HTML files

---
+++ test/testone.sh
@@ -0,0 +1,69 @@
+#! /bin/sh
+
+set -x
+
+INHTML=./input/in_$1.html
+CFGFILE=./input/cfg_$1.txt
+TIDYHTML=./tmp/out_$1.html
+OUTHTML=./output/out_$1.html
+
+MSGFILE=./tmp/msg_$1.txt
+DIFFOUT=./tmp/diff_$1.txt
+
+REPORTWARN=$2
+
+# Remove any pre-exising test outputs
+for INFIL in $MSGFILE $TIDYHTML $DIFFOUT
+do
+  if [ -f $INFIL ]
+  then
+    rm $INFIL
+  fi
+done
+
+# If no test specific config file, use default.
+if [ ! -f $CFGFILE ]
+then
+  CFGFILE=./tidy.cfg
+fi
+
+../tidy -config $CFGFILE $INHTML > $TIDYHTML 2> $MSGFILE
+STATUS=$?
+
+if [ $STATUS -gt 1 ]
+then
+  cat $MSGFILE
+  exit $STATUS
+fi
+
+if [ $REPORTWARN ] 
+then
+  if [ $STATUS -gt 0 ]
+  then
+    cat $MSGFILE
+    exit $STATUS
+  fi
+fi
+
+
+if [ ! -s $TIDYHTML ]
+then
+  cat err.txt
+  exit 1
+fi
+
+if [ -f $OUTHTML ]
+then
+  diff $TIDYHTML $OUTHTML > $DIFFOUT
+
+  # Not a valid shell test
+  if [ -s diff.txt ]
+  then
+    cat err.txt
+    cat diff.txt
+    exit 1
+  fi
+fi
+
+exit 0
+
