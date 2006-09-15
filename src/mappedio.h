2f1f3613cfec48085b213295b129a45c27c4cdfc
arnaud02 <arnaud02>
2006-09-15 16:50:37 +0000

    fix typo

--- src/mappedio.h
+++ src/mappedio.h
@@ -1,18 +1,16 @@
-#ifndef __WIN32TC_H__
-#define __WIN32TC_H__
-#ifdef TIDY_WIN32_MLANG_SUPPORT
+#ifndef __TIDY_MAPPED_IO_H__
+#define __TIDY_MAPPED_IO_H__
 
 /* Interface to mmap style I/O
 
    (c) 2006 (W3C) MIT, ERCIM, Keio University
    See tidy.h for the copyright notice.
 
-   $Id: mappedio.h,v 1.1 2006/09/15 15:45:19 arnaud02 Exp $
+   $Id: mappedio.h,v 1.2 2006/09/15 16:50:37 arnaud02 Exp $
 */
 
 #if defined(_WIN32)
 int TY_(DocParseFileWithMappedFile)( TidyDocImpl* doc, ctmbstr filnam );
 #endif
 
-#endif /* TIDY_WIN32_MLANG_SUPPORT */
-#endif /* __WIN32TC_H__ */
+#endif /* __TIDY_MAPPED_IO_H__ */
