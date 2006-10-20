324f8152383af861a64e01f572038c7ac65c6988
arnaud02 <arnaud02>
2006-10-20 16:44:40 +0000

    http://tidy.sf.net/issue/1581272

--- test/acctest.cmd
+++ test/acctest.cmd
@@ -10,10 +10,57 @@ REM
 REM CVS Info:
 REM
 REM    $Author: arnaud02 $
-REM    $Date: 2006/01/04 14:26:24 $
-REM    $Revision: 1.1 $
+REM    $Date: 2006/10/20 16:44:40 $
+REM    $Revision: 1.2 $
 
-echo Running ACCESS TEST suite > ACCERR.TXT
+@REM USER VARIABLES
+@REM ##############
+@REM set executable to be used
+@set TIDY=..\build\msvc\Release\tidy.exe
+@REM set INPUT folder
+@set TIDYINPUT=accessTest
+@REM set OUTPUT folder
+@set TIDYOUT=tmp
+@REM set input test list file
+@set TIDYIN=accesscases.txt
+@REM ##############
+
+@if NOT EXIST %TIDY% goto ERR1
+@if NOT EXIST %TIDYINPUT%\cfg_default.txt goto ERR2
+@if NOT EXIST %TIDYIN% goto ERR3
+@REM Make sure output directory exists, or WARN of creation ...
+@if EXIST %TIDYOUT%\nul goto RUNTEST
+@echo	NOTE: Folder %TIDYOUT% does not exist. It will be created ...
+@echo	Ctrl+C to abort ... any other keyin to continue ...
+@pause
+@md %TIDYOUT%
+:RUNTEST
+@echo Running ACCESS TEST suite > ACCERR.TXT
+@echo Executable = %TIDY% >> ACCERR.TXT
+@echo Input Folder = %TIDYINPUT% >> ACCERR.TXT
+@echo Output Folder = %TIDYOUT% >> ACCERR.TXT
 set FAILEDACC=
-for /F "tokens=1,2*" %%i in (accesscases.txt) do call onetesta.cmd %%i %%j %%k
+for /F "skip=1 tokens=1,2*" %%i in (%TIDYIN%) do call onetesta.cmd %%i %%j %%k
+@if "%FAILEDACC%." == "." goto SUCCESS
 echo FAILED [%FAILEDACC%] ...
+@goto END
+
+:SUCCESS
+@echo	Appears ALL tests ran fine ...
+@goto END
+
+
+:ERR1
+@echo	ERROR: Unable to locate executable - [%TIDY%] - check name and location ...
+@goto END
+
+:ERR2
+@echo	ERROR: Unable to locate input folder - [%TIDYINPUT%]! - check name and location ...
+@echo	Specifically can not locate the file - [%TIDYINPUT%\cfg_default.txt] ...
+@goto END
+
+:ERR3
+@echo	ERROR: Can not locate file - [%TIDYIN%] - check name and location ...
+@goto END
+
+:END
--- test/onetesta.cmd
+++ test/onetesta.cmd
@@ -10,36 +10,39 @@ REM
 REM CVS Info:
 REM
 REM    $Author: arnaud02 $
-REM    $Date: 2006/01/04 14:26:24 $
-REM    $Revision: 1.1 $
-
-set TIDY=..\bin\tidy
+REM    $Date: 2006/10/20 16:44:40 $
+REM    $Revision: 1.2 $
 
 echo Testing %1 %2 %3
 set TESTNO=%1
 set TESTEXPECTED=%2
 set ACCESSLEVEL=%3
 
-set INFILES=.\accessTest\%1.*ml
-set CFGFILE=.\accessTest\cfg_%1.txt
+set INFILES=%TIDYINPUT%\%1.*ml
+set CFGFILE=%TIDYINPUT%\cfg_%1.txt
 
-set TIDYFILE=.\tmp\out_%1.html
-set MSGFILE=.\tmp\msg_%1.txt
+set TIDYFILE=%TIDYOUT%\out_%1.html
+set MSGFILE=%TIDYout%\msg_%1.txt
 
 set HTML_TIDY=
 
 REM If no test specific config file, use default.
-if NOT exist %CFGFILE% set CFGFILE=.\accessTest\cfg_default.txt
+if NOT exist %CFGFILE% set CFGFILE=%TIDYINPUT%\cfg_default.txt
 
 REM Get specific input file name
 for %%F in ( %INFILES% ) do set INFILE=%%F
 
+if EXIST %INFILE% goto DOIT
+@echo ERROR: Can NOT locate [%INFILE%] ... aborting test ...
+@echo ======================================= >> ACCERR.TXT
+@echo Testing %1 %2 %3 >> ACCERR.TXT
+@echo ERROR: Can NOT locate [%INFILE%] ... aborting test ... >> ACCERR.TXT
+@goto done
+
+:DOIT
 REM Remove any pre-existing test outputs
 if exist %MSGFILE%  del %MSGFILE%
 if exist %TIDYFILE% del %TIDYFILE%
-
-REM Make sure output directory exists
-if NOT exist .\tmp  md .\tmp
 
 REM this has to all one line ...
 %TIDY% -f %MSGFILE% --accessibility-check %ACCESSLEVEL% -config %CFGFILE% --gnu-emacs yes --tidy-mark no -o %TIDYFILE% %INFILE%
@@ -51,7 +54,7 @@ for /F "tokens=3" %%i in (tempres.txt) do set RESULT=%%i
 @REM test the RESULT variable ...
 if "%RESULT%." == "0." goto Err
 if "%RESULT%." == "1." goto done
-echo note - test '%TESTEXPECTED%' found %RESULT% times in file '%INFILE%'
+@REM echo note - test '%TESTEXPECTED%' found %RESULT% times in file '%INFILE%'
 goto done
 
 :Err
