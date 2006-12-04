db0a89059ac00e17a6e517bc172eff7a529d47d2
arnaud02 <arnaud02>
2006-12-04 07:36:20 +0000

    htpp://idy.sf.net/issue/1603538 - better fix for lexer bug

--- src/lexer.c
+++ src/lexer.c
@@ -6,8 +6,8 @@
   CVS Info :
 
     $Author: arnaud02 $ 
-    $Date: 2006/12/03 21:03:30 $ 
-    $Revision: 1.184 $ 
+    $Date: 2006/12/04 07:36:20 $ 
+    $Revision: 1.185 $ 
 
 */
 
@@ -1972,7 +1972,7 @@ Node* TY_(GetToken)( TidyDocImpl* doc, GetTokenMode mode )
     {
         /* duplicate inlines in preference to pushed text nodes when appropriate */
         if ((lexer->token && lexer->token->type != TextNode)
-            || (!lexer->insert && !lexer->inode))
+            || !(lexer->insert || lexer->inode))
         {
             lexer->pushed = no;
             return lexer->token;
@@ -1984,8 +1984,8 @@ Node* TY_(GetToken)( TidyDocImpl* doc, GetTokenMode mode )
 
     if (lexer->insert || lexer->inode)
     {
-        lexer->pushed = no;
-        lexer->token = NULL;
+        if (!lexer->pushed)
+            lexer->token = NULL;
         return TY_(InsertedToken)( doc );
     }
 
@@ -1995,7 +1995,8 @@ Node* TY_(GetToken)( TidyDocImpl* doc, GetTokenMode mode )
         return GetCDATA(doc, lexer->parent);
     }
 
-    lexer->pushed = no;
+    assert( !lexer->pushed );
+    /* Lexer->token must be set on return. Nullify it for safety. */
     lexer->token = NULL;
 
     SetLexerLocus( doc, lexer );
